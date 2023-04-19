d6a340ffa840090a2e05cbf1d0cd749103123863
Veronica <130104127+verocornejo@users.noreply.github.com>
2023-04-19 01:27:10 -0400

    Primer intento

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import validator from './validator.js';
+//declarar variables y llamar desde html
+const button = document.getElementById("validateBtn");
+const creditCard = document.getElementById("inputCard");
 
-console.log(validator);
+
+
+button.addEventListener("click", function(event){ 
+  event.preventDefault();//Evitar que se recargue la página
+  if(creditCard.value === ""){
+    alert("Por favor ingrese su número de tarjeta");
+    return; //termina la función si el input está vacío
+  }
+    
+  const creditCardNumber = creditCard.value;
+  const isValid = validator.isValid(creditCardNumber);
+  const mask = validator.maskify(creditCardNumber);
+
+
+  if(isValid === true){
+    alert("La tarjeta  "+mask+" es válida");
+  }
+  else{
+    alert("La tarjeta  "+mask+" no es válida");
+  }
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,33 @@
 const validator = {
-  // ...
+  isValid: function (creditCardNumber) {
+    const creditCardArray = creditCardNumber.split("").reverse(); //metodo split transforma string en array y reverse invierte los elementos
+    let total = 0; 
+  
+    for (let i = 0; i < creditCardArray.length; i++) {
+      let currentValue = Number(creditCardArray[i]); //number es un objeto primitivo envolvente que permite representar y manipular valores numéricos 
+  
+      if (i % 2 !== 0) {
+        currentValue *= 2; //si el indice es par, multiplicar por dos
+  
+        if (currentValue > 9) { //si el valor del indice es mayor que 9, sumar los digitos
+          const digits = currentValue.toString().split(""); //pasar a string
+          currentValue = Number(digits[0]) + Number(digits[1]); 
+        }
+      }
+  
+      total += currentValue;
+    }
+  
+    return total % 10 === 0;
+  },
+  
+  maskify: function (creditCardNumber) {
+    const maskedNumber = creditCardNumber.slice(0, -4).replace(/./g, "#"); //metodo slice para tomar solo una parte del indice. /g es un modificador global (regular exp) se combina con replace 
+    return maskedNumber + creditCardNumber.slice(-4); //-4 porque es de atras para adelante
+  },
 };
-
+  
 export default validator;
+  
+  
+

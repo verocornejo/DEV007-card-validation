49d370657c9f0e0a122c3051c6d059dfcbbdb6a6
Veronica <130104127+verocornejo@users.noreply.github.com>
2023-04-24 12:56:53 -0400

    prueba commit 2

--- src/index.js
+++ src/index.js
@@ -4,28 +4,39 @@ const button = document.getElementById("validateBtn");
 const creditCard = document.getElementById("inputCard");
 
 
-
 button.addEventListener("click", function(event){ 
   event.preventDefault();//Evitar que se recargue la página
   if(creditCard.value === ""){
     alert("Por favor ingrese su número de tarjeta");
     return; //termina la función si el input está vacío
   }
-    
+  
+
+  
   const creditCardNumber = creditCard.value;
   const isValid = validator.isValid(creditCardNumber);
   const mask = validator.maskify(creditCardNumber);
 
 
   if(isValid === true){
-    alert("La tarjeta  "+mask+" es válida");
+    alert("La tarjeta  "+ mask +" es válida");
   }
   else{
-    alert("La tarjeta  "+mask+" no es válida");
+    alert("La tarjeta  "+ mask+" no es válida");
   }
 }); 
-
-
+//para que el input sean solo numeros
+window.onlyNum = function onlyNum(event) {
+  const key = event.key;
+  if(!key.match (/\d|Backspace/))
+    event.preventDefault();
+};
+//para que el input sea solo letras
+window.onlyTxt = function onlyTxt(event) {
+  const key = event.key;
+  if(!key.match (/^[A-Za-z\s]*$/))
+    event.preventDefault();
+};
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,32 +1,34 @@
 const validator = {
-  isValid: function (creditCardNumber) {
-    const creditCardArray = creditCardNumber.split("").reverse(); //metodo split transforma string en array y reverse invierte los elementos
+  isValid: function (creditCardNumber) { //funcion que recibe como parametro un numero de tarjeta
+    const creditCardArray = creditCardNumber.split("").reverse(); // convertir el numero (string) en un array al usar metodo split transforma string en array y reverse invierte los elementos
+    console.log(creditCardArray);
     let total = 0; 
-  
+    
     for (let i = 0; i < creditCardArray.length; i++) {
       let currentValue = Number(creditCardArray[i]); //number es un objeto primitivo envolvente que permite representar y manipular valores numéricos 
-  
+      console.log(currentValue);
       if (i % 2 !== 0) {
         currentValue *= 2; //si el indice es par, multiplicar por dos
   
         if (currentValue > 9) { //si el valor del indice es mayor que 9, sumar los digitos
-          const digits = currentValue.toString().split(""); //pasar a string
+          const digits = currentValue.toString().split(""); //pasar a array
           currentValue = Number(digits[0]) + Number(digits[1]); 
         }
       }
-  
+      
       total += currentValue;
     }
   
     return total % 10 === 0;
   },
+
   
   maskify: function (creditCardNumber) {
     const maskedNumber = creditCardNumber.slice(0, -4).replace(/./g, "#"); //metodo slice para tomar solo una parte del indice. /g es un modificador global (regular exp) se combina con replace 
     return maskedNumber + creditCardNumber.slice(-4); //-4 porque es de atras para adelante
-  },
+  }, 
 };
-  
+
 export default validator;
   
   
